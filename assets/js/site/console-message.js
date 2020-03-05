8f8e2cba42661b83b4218f7d44d24bc1b8179922
Lisa Chung <31673962+lisac-usds@users.noreply.github.com>
2020-03-05 13:44:12 -0500

    fix js bugs

--- assets/js/site/console-message.js
+++ assets/js/site/console-message.js
@@ -4,9 +4,9 @@ $(document).ready(function () {
 		console.log(' _    _  _____ _____   _____ ');
 		console.log('| |  | |/ ____|  __ \ / ____|');
 		console.log('| |  | | (___ | |  | | (___    usds.gov/apply');
-		console.log('| |  | |\___ \| |  | |\___ \ );
+		console.log('| |  | |\___ \| |  | |\___ \ ');
 		console.log('| |__| |____) | |__| |____) |');
 		console.log(' \____/|_____/|_____/|_____/ ');
 		console.log(' ');
 	}
-}
+});
