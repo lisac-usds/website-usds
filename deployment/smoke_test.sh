06395e14232ff71627ffcc3ac33be3e9f5a4782e
Lisa Chung <31673962+lisac-usds@users.noreply.github.com>
2020-03-06 07:28:20 -0500

    Create smoke_test.sh

---
+++ deployment/smoke_test.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+echo "Starting tests..."
+
+tests_count=0
+failures_count=0
+
+declare -a pages
+pages=("index.html" "mission.html" "apply.html" "people.html")
+for p in "${pages[@]}"; do
+    if ! test -e "../_site/$p" ; then
+        echo "page $p does not exist"
+        (( ++failures_count ))
+    fi
+    (( ++tests_count ))
+done
+
+
+echo "Finished tests. Failed $failures_count of $tests_count tests."
+
+if [[ $failures_count -ne 0 ]]; then
+  exit 1
+fi
